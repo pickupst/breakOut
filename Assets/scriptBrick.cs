dca2877df0d8f0763fffbf8610fdede075ce2094
pickupst <gd.okanuslu@gmail.com>
2019-01-19 21:31:55 +0300

    Add bricks

---
+++ Assets/scriptBrick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptBrick : MonoBehaviour
+{
+    static int numberBrick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numberBrick++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+        numberBrick--;
+
+        if (numberBrick <= 0)
+        {
+            Debug.Log("GAME OVER");
+        }
+    }
+}
