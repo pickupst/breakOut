352eb7daaca085513726fbeac7fed0d2dc641473
pickupst <gd.okanuslu@gmail.com>
2019-01-19 20:47:44 +0300

    Add a ball
    Create ball physics and game walls

--- Assets/scriptCube.cs
+++ Assets/scriptCube.cs
@@ -6,16 +6,28 @@ public class scriptCube : MonoBehaviour
 {
     float zamanKatsayisi;
 
+    public GameObject prefabBall;
+
+    GameObject attachedBall;
     // Start is called before the first frame update
     void Start()
     {
-        
+        attachedBall = (GameObject) Instantiate(prefabBall, transform.position + new Vector3(0, 0.2f, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
         zamanKatsayisi = Time.deltaTime;
+
+        attachedBall.GetComponent<Rigidbody>().AddForce(0, 20f, 0);
+
+        move();
+
+    }
+
+    private void move()
+    {
 
         transform.Translate(Input.GetAxis("Horizontal") * 10f * zamanKatsayisi, 0, 0);
 
