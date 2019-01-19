413f4dadd395f338cfaad627a049196ffe669154
pickupst <gd.okanuslu@gmail.com>
2019-01-19 20:18:41 +0300

    İnitial Commit
    Create cube and add moveing action in first commit

---
+++ Assets/scriptCube.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptCube : MonoBehaviour
+{
+    float zamanKatsayisi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        zamanKatsayisi = Time.deltaTime;
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            transform.Translate(-10f * zamanKatsayisi, 0, 0);
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.Translate(10f * zamanKatsayisi, 0, 0);
+        }
+
+    }
+}
